ebc139dca303dab9f78bec7fa3407defd9d9c422
Abhijat05 <abhijatsinha2004@gmail.com>
2025-02-27 11:56:32 +0530

    add routing for Home, Inventory, and About pages in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import InventoryPage from './pages/InventoryPage'
+import AboutPage from './pages/AboutPage'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <h1>Home Page</h1>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-100 dark:bg-gray-800">
+        <Navbar />
+        <div className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/inventory" element={<InventoryPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   )
 }
 
