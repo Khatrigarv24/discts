2eb631239a87497fb47af7146a2307dce8074732
khatrigarv24 <gk6504@srmist.edu.in>
2025-03-02 23:39:18 +0530

    commit

--- backend/src/api/inventory/inventory-services.ts
+++ backend/src/api/inventory/inventory-services.ts
@@ -1,21 +1,17 @@
-import {
-  DynamoDBClient,
-  CreateTableCommand,
-  ListTablesCommand,
-} from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-import dotenv from "dotenv";
+import { DynamoDBClient, CreateTableCommand, ListTablesCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import dotenv from 'dotenv';
 
 dotenv.config();
 // Create DynamoDB Client
 const dbClient = new DynamoDBClient({
-  region: process.env.AWS_REGION as string,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY as string,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-    sessionToken: process.env.AWS_SESSION_TOKEN as string,
-  },
-});
+    region: process.env.AWS_REGION as string,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY as string,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+      sessionToken: process.env.AWS_SESSION_TOKEN as string,
+    },
+  });
 
 const ddbDocClient = DynamoDBDocumentClient.from(dbClient);
 
@@ -24,22 +20,10 @@ export async function connectDB(): Promise<void> {
   try {
     // Check if table already exists
     const listTables = await dbClient.send(new ListTablesCommand({}));
-    if (listTables.TableNames?.includes("discts")) {
-      console.log("✅ Inventory table exists");
+    if (listTables.TableNames?.includes('discts')) {
+      console.log("✅ Discts table exists");
       return;
     }
-
-    // Create Table if Not Exists
-    await dbClient.send(
-      new CreateTableCommand({
-        TableName: "Inventory",
-        KeySchema: [{ AttributeName: "productId", KeyType: "HASH" }],
-        AttributeDefinitions: [
-          { AttributeName: "productId", AttributeType: "S" },
-        ],
-        BillingMode: "PAY_PER_REQUEST",
-      })
-    );
 
     console.log("✅ Inventory table created successfully");
   } catch (err) {
